32c6b319a6856858bb711a695856ee79146de407
JacksonMalloy <jacksmalloy@gmail.com>
2019-12-31 10:55:40 -0800

    initialized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Scanner from "zbar.wasm";
+import "./App.css";
+
+const loadImage = async src => {
+  //Load image
+  const imgBlob = await fetch(src).then(resp => resp.blob());
+  const img = await createImageBitmap(imgBlob);
+
+  //Make canvas same size as image
+  const canvas = document.createElement("canvas");
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  //Draw image on to Canvas
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+
+  return {
+    imageData: ctx.getImageData(0, 0, img.width, img.height),
+    width: img.width,
+    height: img.height
+  };
+};
+
+const main = async () => {
+  //Create a Scanner Object
+
+  // Add version tag if in production
+  const wasm_file_path = "https://cdn.jsdelivr.net/npm/zbar.wasm/data/";
+  const scanner = await Scanner({
+    locateFile: file => wasm_file_path + file
+  });
+
+  const { imageData, width, height } = await loadImage("URL_TO_IMAGE");
+  const scanRes = scanner.scanQrcode(imageData, width, height);
+
+  if (scanRes.length > 0) {
+    console.log("Find Qrcode: " + scanRes);
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => main()}>Click Me</button>
     </div>
   );
 }
